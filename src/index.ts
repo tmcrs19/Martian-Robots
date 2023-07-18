b9373f5d642d697025873558e08706420036d39d
Theo <tmacris@cloudbees.com>
2023-07-18 15:50:32 +0300

    feat: main

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as fs from "fs";
+import { createRobot } from "./Robot";
+import { createMarsMission } from "./MarsMission";
+
+const inputFilePath = "test-data.dat";
+
+let array: string[] = [];
+let mission: ReturnType<typeof createMarsMission> | undefined;
+let robot: ReturnType<typeof createRobot> | null = null;
+let instructions = "";
+
+try {
+  array = fs.readFileSync(inputFilePath).toString().split("\n");
+  console.log(array);
+  mission = createMarsMission(array);
+
+  robot = mission.nextRobot();
+  while (robot !== null) {
+    instructions = mission.nextRobotInstructions();
+
+    robot.processInstructions(instructions);
+
+    const isLost = robot.isLost() ? "LOST" : "";
+    console.log(
+      `${robot.getX()} ${robot.getY()} ${robot.getFacing()} ${isLost}`
+    );
+    robot = mission.nextRobot();
+  }
+} catch (error) {
+  console.error((error as Error).message);
+}
